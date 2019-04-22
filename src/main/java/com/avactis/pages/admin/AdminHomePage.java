80ce3f5e268d07ef698c5de350c374ceee930f2e
Amol <amolkolpawar@isynergysys.com>
2019-04-22 12:29:22 +0530

    New Commite

--- src/main/java/com/avactis/pages/admin/AdminHomePage.java
+++ src/main/java/com/avactis/pages/admin/AdminHomePage.java
@@ -1,11 +1,8 @@
 package com.avactis.pages.admin;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptException;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -66,7 +63,7 @@ public class AdminHomePage {
 			Thread.sleep(10000);
 			action.moveToElement(select).click().build().perform();
 			
-			String parent = driver.getWindowHandle();
+			//String tesging = driver.getWindowHandle();
 			String child = driver.getWindowHandle();
 			
 			driver.switchTo().window(child);
